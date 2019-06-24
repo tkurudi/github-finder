46ce0a281c163189eac12af5462a75905d78d083
tkurudi <t.kurudi209@gmail.com>
2019-06-24 15:55:27 +1200

    added props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render(){
   return (
     <div className="App">
       <Navbar title='Github Finder' icon='fab fa-github'/>
+      <Users/>
     </div>
   );
  }
