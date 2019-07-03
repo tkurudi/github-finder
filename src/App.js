c0016afccdedf2f5f26102b0d610705b8de86195
tkurudi <t.kurudi209@gmail.com>
2019-07-03 17:43:34 +1200

    link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import axios from 'axios';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -47,18 +49,28 @@ setTimeout(() => this.setState({alert: null}), 5000)
   render(){
     const {users, loading} = this.state
   return (
+    <Router>
     <div className="App">
       <Navbar title='Github Finder' icon='fab fa-github'/>
       <div className="container">
       <Alert alert={this.state.alert}/>
-      <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
+      <Switch>
+        <Route exact path='/' render={props => (
+          <Fragment>
+             <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
         users.length > 0 ? true: false}
         setAlert={this.setAlert}
       />
       <Users loading={loading} users={users}/>
       
+          </Fragment>
+        )} />
+        <Route exact path='/about' component={About}/>
+      </Switch>
+     
       </div>
     </div>
+    </Router>
   );
  }
 }
